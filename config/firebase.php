00c8cc942d0b22b2199b2607c65ec72bd0c2f271
vtp168 <vuthyphon68@gmail.com>
2025-12-15 15:47:24 +0700

    updated fix credentials

--- config/firebase.php
+++ config/firebase.php
@@ -3,6 +3,8 @@
 declare(strict_types=1);
 
 return [
+
+    'credentials' => env('FIREBASE_CREDENTIALS'),
     /*
      * ------------------------------------------------------------------------
      * Default Firebase project
